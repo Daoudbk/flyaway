090f9e21f1ea53c0129d5408c0a577b8f1405c1d
daniil.moskovtsov <daniil.moskovtsov@dsquare.be>
2015-01-16 17:33:23 +0100

    Fixing cors.

--- server/server.js
+++ server/server.js
@@ -2,11 +2,13 @@ var express = require('express');
 var http = require('http');
 var path = require('path');
 var search = require('./routes/search');
+var cors = require('cors');
 
 var app = express();
 
 app.configure(function () {
     app.set('port', 3000);
+    app.use(cors())
     app.use(express.bodyParser());
     app.use(express.methodOverride());
     app.use(app.router);
